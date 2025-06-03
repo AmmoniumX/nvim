0068c71a533f39b0de3dc676a1f8a20472d74bc4
Diego Garcia <diego.garcia.cr@gmail.com>
2025-06-03 14:35:02 +0200

    added obsidian.nvim

--- init.lua
+++ init.lua
@@ -1,11 +1,18 @@
 require("config.lazy")
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+-- Restore default cursor on exit
 vim.api.nvim_create_autocmd("VimLeave", {
   pattern = "*",
   command = "set guicursor=a:ver25",
 })
-vim.opt.wrap = false
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-
+-- Format python files
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.py",
+  callback = function()
+      vim.cmd("!ruff format %")
+  end,
+})
