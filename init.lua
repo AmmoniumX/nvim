b5c345558522c2bd543e75bdf032d61f2c46aad4
Diego Garcia <diego.garcia.cr@gmail.com>
2025-07-09 12:32:27 -0600

    don't touch cursor after exiting

--- init.lua
+++ init.lua
@@ -9,11 +9,6 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 vim.opt.conceallevel = 1
 
--- Restore default cursor on exit
-vim.api.nvim_create_autocmd("VimLeave", {
-  pattern = "*",
-  command = "set guicursor=a:ver25",
-})
 -- Format python files
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.py",
