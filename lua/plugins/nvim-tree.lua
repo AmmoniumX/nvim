35d3727c158aace283b1733416eeafef719acbe2
Diego Garcia <diego.garcia.cr@gmail.com>
2025-07-09 12:22:59 -0600

    added telescope.lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,5 +7,9 @@ return {
   },
   config = function()
     require("nvim-tree").setup {}
+    -- Set keybinding for 'fm' using nvim-tree's built-in API
+    vim.keymap.set('n', '<leader>fm', function()
+      require('nvim-tree.api').tree.open()
+    end, { noremap = true, silent = true })
   end,
 }
