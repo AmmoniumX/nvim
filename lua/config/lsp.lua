7f60d29275936faa6f144cdd6f118e714879b5eb
Diego Garcia <diego.garcia.cr@gmail.com>
2025-09-25 20:33:05 -0600

    add typescript-language-server

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -2,7 +2,7 @@
 vim.pack.add({{ src = 'https://github.com/neovim/nvim-lspconfig' }})
 require('lspconfig')
 
-vim.lsp.enable({'rust_analyzer', 'clangd', 'pyright', 'lua_ls'})
+vim.lsp.enable({'rust_analyzer', 'clangd', 'pyright', 'lua_ls', 'ts_ls'})
 
 -- Get the language server to recognize the `vim` global
 vim.lsp.config('lua_ls', {
