6f9b7965fe9ec1ce558d568c30763c9f73954c6d
Diego Garcia <diego.garcia.cr@gmail.com>
2025-10-19 00:18:05 -0600

    Remove GeminiAccept/Reject keybinds

--- lua/plugins/nvim-gemini-companion.lua
+++ lua/plugins/nvim-gemini-companion.lua
@@ -12,12 +12,4 @@ vim.keymap.set('n', '<leader>gg', function()
     vim.cmd("GeminiToggle")
 end, { desc = 'Toggle Gemini Cli' })
 
-vim.keymap.set('n', '<leader>ga', function()
-    vim.cmd("GeminiAccept")
-end, { desc = 'Accept Gemini Changes' })
-
-vim.keymap.set('n', '<leader>gr', function()
-    vim.cmd("GeminiReject")
-end, { desc = 'Reject Gemini Changes' })
-
 vim.keymap.set('t', '<C-X>', '<C-\\><C-N>', { desc = "Enter normal mode from terminal" })
