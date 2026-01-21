4e299ad71d9a25cfe2bc2612071a6c41a70606f6
Diego Garcia <diego.garcia.cr@gmail.com>
2026-01-21 13:57:08 -0600

    add ToggleWriteFormat, SetWriteFormat, and PackUpdate user cmds

--- init.lua
+++ init.lua
@@ -26,21 +26,67 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function() vim.treesitter.start() end,
 })
 
--- Format python files
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*.py",
-  callback = function()
-      vim.cmd("!ruff format %")
-  end,
-})
+--- Format-On-Write ---
 
--- Format C++ files
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.cpp", "*.hpp" },
-  callback = function()
-      vim.cmd("!clang-format --style=llvm -i % ")
-  end,
-})
+-- Create a variable to track the state
+vim.g.writeformat_enabled = true
+
+-- Function to set up the autocmds
+local function setup_format_autocmds()
+  -- Clear any existing autocmds in this group
+  vim.api.nvim_create_augroup("WriteFormat", { clear = true })
+
+  if vim.g.writeformat_enabled then
+    -- Format python files
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      group = "WriteFormat",
+      pattern = "*.py",
+      callback = function()
+        vim.cmd("!ruff format %")
+      end,
+    })
+
+    -- Format C++ files
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      group = "WriteFormat",
+      pattern = { "*.cpp", "*.hpp" },
+      callback = function()
+        vim.cmd("!clang-format --style=llvm -i %")
+      end,
+    })
+  end
+end
+
+-- Initial setup
+setup_format_autocmds()
+
+-- Toggle command
+vim.api.nvim_create_user_command("ToggleWriteFormat", function()
+  vim.g.writeformat_enabled = not vim.g.writeformat_enabled
+  setup_format_autocmds()
+  print("Write format: " .. (vim.g.writeformat_enabled and "enabled" or "disabled"))
+end, {})
+
+-- Set command
+vim.api.nvim_create_user_command("SetWriteFormat", function(opts)
+  if opts.args == "on" then
+    vim.g.writeformat_enabled = true
+  elseif opts.args == "off" then
+    vim.g.writeformat_enabled = false
+  end
+  setup_format_autocmds()
+  print("Write format: " .. (vim.g.writeformat_enabled and "enabled" or "disabled"))
+end, { nargs = 1, complete = function() return {"on", "off"} end })
+
+vim.api.nvim_create_user_command("PackUpdate", function(opts)
+  if opts.args ~= "" then
+    local names = vim.split(opts.args, "%s+")
+    vim.pack.update(names)
+  else
+    -- Update all packages - pass empty list as first argument
+    vim.pack.update({})
+  end
+end, { nargs = "*" })
 
 -- Configure split options
 vim.opt.splitright = true
